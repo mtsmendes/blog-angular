4373081612f8766ca95a5dc57b1aff7a1f0a4442
Mendes <MarcoTulioSilveira.Mendes@philips.com>
2023-10-18 08:42:52 -0300

    components created with styles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HComponent },
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
